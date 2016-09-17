dc32e6550071297a8483bb5ba361e21634653fed
elmarcus <marcus.blatter@gmail.com>
2016-09-17 15:59:03 -0600

    Add files via upload
    Java docs

---
+++ src/map/Map.java
@@ -0,0 +1,12 @@
+package map;
+
+public class Map {
+	
+	/**
+	 * for each hex that has integer diceRoll as a hexValue, 
+	 * the resource of the hex will be given to each player that is on an adjacent hex intersection
+	 * @param diceRoll integer that dictates which hex will produce resources this round. Dice roll must be between 2-12
+	 */
+	public void gatherResources(Integer diceRoll){}
+
+}
---
+++ src/player/ResourceList.java
@@ -0,0 +1,11 @@
+package player;
+
+public class ResourceList {
+	
+	/**
+	 * checks if resource list contains at least all of the resources present in the inputList
+	 * @param inputList resourceList to compare this resource list against, inputList must be non-empty
+	 * @return true if all resource amounts found in inputList are found in this resource list
+	 */
+	public boolean hasResource(ResourceList inputList){return true;}
+}
