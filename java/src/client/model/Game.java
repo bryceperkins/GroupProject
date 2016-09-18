8c902be144917aa80b3e08f6949f2b482fe370a1
Joseph Davison <josephddavison@gmail.com>
2016-09-17 18:38:56 -0600

    Added initial model stubbing & javadocs

---
+++ java/src/client/model/Game.java
@@ -0,0 +1,73 @@
+package client.model;
+
+import java.util.HashMap;
+
+/**
+ * Game model class containing all local information provided by
+ * the server for a given game.
+ */
+public class Game {
+
+    private String name;
+    private int version;
+    private PlayerIndex winner;
+    private ResourceBank bank;
+    private Chat chat;
+    private Log log;
+    private Map map;
+    private HashMap<String, Player> players;
+    private TurnTracker turnTracker;
+    private TradeOffer tradeOffer;
+
+    /**
+     * @return whether the game is completed
+     */
+    public boolean gameOver() {}
+
+    /**
+     * @return whether the game is ready to begin
+     */
+    public boolean canBeginGame() {}
+
+    public String getName() {
+        return name;
+    }
+
+    public int getVersion() {
+        return version;
+    }
+
+    public PlayerIndex getWinner() {
+        return winner;
+    }
+
+    public ResourceBank getBank() {
+        return bank;
+    }
+
+    public Chat getChat() {
+        return chat;
+    }
+
+    public Log getLog() {
+        return log;
+    }
+
+    public Map getMap() {
+        return map;
+    }
+
+    public HashMap<String, Player> getPlayers() {
+        return players;
+    }
+
+    public TurnTracker getTurnTracker() {
+        return turnTracker;
+    }
+
+    public TradeOffer getTradeOffer() {
+        return tradeOffer;
+    }
+
+
+}
