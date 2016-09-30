8390e7ae1a1f9fd39cb5932f9198c5abca62df99
elmarcus <marcus.blatter@gmail.com>
2016-09-30 16:50:16 -0600

    map object method development

--- java/src/client/model/map/Hex.java
+++ java/src/client/model/map/Hex.java
@@ -1,5 +1,43 @@
 package client.model.map;
 
-public abstract class Hex {
+import client.model.ResourceType;
 
+public class Hex {
+	
+	private HexLocation location;
+	private Robber robber;
+	private int value;
+	private ResourceType resource;
+	
+	public boolean hasRobber()
+	{
+		if(robber == null)
+			return false;
+		else
+			return true;
+	}
+	
+	public boolean canPlaceRobber()
+	{
+		return !this.hasRobber();
+	}
+
+	public HexLocation getLocation() {
+		return location;
+	}
+
+	public Robber getRobber() {
+		return robber;
+	}
+
+	public int getValue() {
+		return value;
+	}
+
+	public ResourceType getResource() {
+		return resource;
+	}
+	
+	
+	
 }
