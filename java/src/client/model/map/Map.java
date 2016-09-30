6721781a59b90b9140de6339ba72cfecf0a6fb81
bryceperkins <none@none>
2016-09-29 18:28:58 -0600

    setup files

--- java/src/client/model/map/Map.java
+++ java/src/client/model/map/Map.java
@@ -51,4 +51,5 @@ public class Map {
     public void setRobber(Robber robber) {
         this.robber = robber;
     }
+
 }
--- java/src/test/MapTest.java
+++ java/src/test/MapTest.java
@@ -1,56 +1,73 @@
+/*package test;
+
 import static org.junit.Assert.*;
 
 import org.junit.Test;
 import org.junit.Before;
 import org.junit.After;
 
-import client.model.map.*;
-import client.model.map.Map;
-import client.model.map.ItemLocation;
-import client.model.map.Direction;
 import client.model.*;
+import client.model.TradeOffer.TradeOfferResourceList;
 import client.model.player.*;
-import client.model.ResourceList;
 
-public class MapTest {
-    private Map testMap;
-    private ResourceList superList;
+public class PlayerTest {
     private Player testPlayer;
-    private HexLocation hexLocation;
-    private Direction direction;
-    private ItemLocation itemLocation;
-
+    private ResourceList superList;
     
     @Before
     public void setUp()
     {
-        testPlayer = new Player(Color.green,"Alice", 1, PlayerIndex.Player1 ,1);
-        hexLocation = new HexLocation(1,1);
-        direction = Direction.N;
-        itemLocation = new ItemLocation(hexLocation,direction);
-        testMap = new Map(); 
+        testPlayer = new Player("Alice", Color.puce, 1);
+        superList = new ResourceList();
+        superList.setBrick(1000);
+        superList.setWheat(1000);
+        superList.setWood(1000);
+        superList.setSheep(1000);
+        superList.setOre(1000);
     }
     
     @After
     public void tearDown()
     {
+        testPlayer = new Player("Alice", Color.puce, 1);
     }
     
     @Test
-    public void test_canBuildCity_expect_true()
+    public void test_canBuildCity_expect_false()
     {
-        assertEquals(true, testMap.canBuildCity(testPlayer,itemLocation));
+        assertEquals(false, testPlayer.canBuildCity());
     }
     
     @Test
     public void test_canBuildSettlement_expect_true()
     {
-        assertEquals(true, testMap.canBuildSettlement(testPlayer,itemLocation));
+        testPlayer.addResources(superList);
+        assertEquals(true, testPlayer.canBuildCity());
     }
     
     @Test
-    public void test_canBuildRoad_expect_true()
+    public void test_canBuildRoad_expect_false()
     {
-        assertEquals(true, testMap.canBuildRoad(testPlayer,itemLocation));
-    } 
-}
+        assertEquals(false, testPlayer.canBuildRoad());
+    }
+    
+    @Test
+    public void test_canBuyDevCard_expect_true()
+    {
+        testPlayer.addResources(superList);
+        assertEquals(true, testPlayer.canBuyDevCard());
+    }
+    
+    @Test
+    public void test_canMakeTrade_expect_true()
+    {
+        testPlayer.addResources(superList);
+        TradeOffer trade_offer = new TradeOffer();
+
+        ResourceList emptyList = new ResourceList();
+        trade_offer.setOffer(emptyList);
+        
+        assertEquals(true, testPlayer.canMakeTrade(trade_offer));
+    }
+    
+}*/
