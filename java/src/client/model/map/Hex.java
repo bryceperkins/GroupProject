5fb7bf6a333ee51b235d9e4cb5cf74e524ce54d5
Me <brycejp@byu.edu>
2016-09-30 09:59:39 -0600

    tests

--- java/src/client/model/map/Hex.java
+++ java/src/client/model/map/Hex.java
@@ -5,49 +5,20 @@ import client.model.map.Robber;
 import client.model.ResourceType;
 
 public class Hex {
-    private HexLocation location;
-    //private Robber robber;
-    private boolean hasRobber;
-    private int value;
-    private ResourceType resource;
+	private HexLocation location;
+	//private Robber robber;
+	private boolean hasRobber;
+	private int value;
+	private ResourceType resource;
 
-    public Hex() {}
+	public Hex(HexLocation location, boolean hasRobber, int value, ResourceType resource){
+		this.location = location;
+		this.hasRobber = hasRobber;
+		this.value = value;
+		this.resource = resource;
+	};
 
-    public Hex(HexLocation location, boolean hasRobber, int value, ResourceType resource){};
+	public boolean hasRobber(){return true;}
 
-    public HexLocation getLocation() {
-        return location;
-    }
-
-    public void setLocation(HexLocation location) {
-        this.location = location;
-    }
-
-    public boolean isHasRobber() {
-        return hasRobber;
-    }
-
-    public void setHasRobber(boolean hasRobber) {
-        this.hasRobber = hasRobber;
-    }
-
-    public int getValue() {
-        return value;
-    }
-
-    public void setValue(int value) {
-        this.value = value;
-    }
-
-    public ResourceType getResource() {
-        return resource;
-    }
-
-    public void setResource(ResourceType resource) {
-        this.resource = resource;
-    }
-
-    public boolean hasRobber(){return true;}
-
-    public boolean canPlaceRobber(){return true;}
+	public boolean canPlaceRobber(){return true;}
 }
--- java/src/client/model/map/Port.java
+++ java/src/client/model/map/Port.java
@@ -6,51 +6,17 @@ import client.model.ResourceList;
 
 public class Port {
 
-    private ResourceType resource;
-    private HexLocation location;
-    private int ratio;
-    private Direction direction;
+	private ResourceType resource;
+	private HexLocation location;
+	private int ratio;
+	private Direction direction;
 
-    public void setResource(ResourceType resource) {
-        this.resource = resource;
-    }
+	public Port(ResourceType resource, HexLocation location, int ratio, Direction direction){
+		this.resource = resource;
+		this.location = location;
+		this.ratio = ratio;
+		this.direction = direction;
+	};
 
-    public void setLocation(HexLocation location) {
-        this.location = location;
-    }
-
-    public void setRatio(int ratio) {
-        this.ratio = ratio;
-    }
-
-    public void setDirection(Direction direction) {
-        this.direction = direction;
-    }
-
-    public Port() {}
-
-    public Port(ResourceType resource, HexLocation location, int ratio, Direction direction){
-        this.resource = resource;
-        this.location = location;
-        this.ratio = ratio;
-        this.direction = direction;
-    };
-
-    public boolean canTrade(ResourceList resourceList){return true;}
-
-    public ResourceType getResource() {
-        return resource;
-    }
-    
-    public HexLocation getLocation() {
-        return location;
-    }
-    
-    public int getRatio() {
-        return ratio;
-    }
-    
-    public Direction getDirection() {
-        return direction;
-    }
+	public boolean canTrade(ResourceList resourceList){return true;}
 }
