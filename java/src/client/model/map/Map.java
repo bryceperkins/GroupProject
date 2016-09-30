4243672533ef877a31d72b4bd2df7ca7676042f6
elmarcus <marcus.blatter@gmail.com>
2016-09-30 17:07:06 -0600

    removed dumb gatherResources function that isn't necessary

--- java/src/client/model/map/Map.java
+++ java/src/client/model/map/Map.java
@@ -116,22 +116,5 @@ public class Map {
 		}
 		return false;
 	}
-	
-	
-	/**
-	 * for each hex that has integer diceRoll as a hexValue, 
-	 * the resource of the hex will be given to each player that is on an adjacent hex intersection
-	 * @param diceRoll integer that dictates which hex will produce resources this round. Dice roll must be between 2-12
-	 */
-	public void gatherResources(Integer diceRoll){
-		
-		ArrayList<Hex> hexToGiveResource = hexByTileValue.get(diceRoll);
-		
-		for(int i = 0; i < hexToGiveResource.size(); i++)
-		{
-			hexToGiveResource.get(i).getResource();
-		}
-		
-	}
 
 }
