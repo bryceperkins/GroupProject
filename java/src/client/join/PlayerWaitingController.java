d949bb8476af2e4eeb3486f8cc3daf1575e6847d
Aaron Bishop <abishop@linux.com>
2016-11-02 01:01:55 -0600

    Fixed the bug Joe Found ... maybe? I think?

--- java/src/client/join/PlayerWaitingController.java
+++ java/src/client/join/PlayerWaitingController.java
@@ -33,7 +33,7 @@ public class PlayerWaitingController extends Controller implements IPlayerWaitin
         Gson gson = new Gson();
         this.active = true;
         this.manager.addObserver(this);
-        update(null, null);
+        addPlayers();
 
         String response = this.manager.getServer().execute(new GameListAI());
         String[] ais = gson.fromJson(response, String[].class);
@@ -50,18 +50,24 @@ public class PlayerWaitingController extends Controller implements IPlayerWaitin
         String response = this.manager.getServer().execute(new GameAddAI(AIType.valueOf(aitype)));
 	}
 
-    public void update(Observable ob, Object o){
+    public void addPlayers(){
         this.game = manager.getGame(manager.getServer().getServer().getDetails().getGameID());
         int size = this.game.getPlayers().size();
         PlayerInfo[] players = new PlayerInfo[size];
-
         for(int i=0; i < size; i++){
             Player p = this.game.getPlayers().get(i);
             players[i] = p.toPlayerInfo();
         }
         getView().setPlayers(players);
+    }
+
+    public void update(Observable ob, Object o){
+        getView().closeModal();
         if(this.game.canBeginGame()){
             startGame();
+        }
+        else{
+            start();
         }
     }
 
