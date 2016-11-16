f8ab391f6c623935b8f1a7210befef9fde406753
SolarFlar3 <jacobanderson1104@gmail.com>
2016-11-15 21:42:22 -0700

    Did a lot of work on the map for create game

--- java/src/shared/model/map/Map.java
+++ java/src/shared/model/map/Map.java
@@ -5,6 +5,7 @@ import java.util.*;
 import shared.model.player.*;
 import shared.model.*;
 import shared.locations.*;
+import shared.definitions.ResourceType;
 
 public class Map implements PostProcessor {
 
@@ -16,8 +17,162 @@ public class Map implements PostProcessor {
     private int radius;
     private Robber robber;
 
+	//Components of Catan, used for randomization
+	private HexLocation[] portLocations = {
+		new HexLocation(-3,  0),
+		new HexLocation(-1, -2),
+		new HexLocation( 1, -3),
+		new HexLocation( 3, -3),
+		new HexLocation( 3, -1),
+		new HexLocation( 2,  1),
+		new HexLocation( 0,  3),
+		new HexLocation(-2,  3),
+		new HexLocation(-3,  2),
+	};
+
+	private final EdgeDirection[] portDirections = {
+		EdgeDirection.SouthEast,
+		EdgeDirection.South,
+		EdgeDirection.South,
+		EdgeDirection.SouthWest,
+		EdgeDirection.NorthWest,
+		EdgeDirection.NorthWest,
+		EdgeDirection.North,
+		EdgeDirection.NorthEast,
+		EdgeDirection.NorthEast,
+	};
+	
+	private transient final ResourceType[] portTypes = {
+		null,
+		ResourceType.WHEAT,
+		ResourceType.ORE,
+		null,
+		ResourceType.SHEEP,
+		null,
+		null,
+		ResourceType.BRICK,
+		ResourceType.WOOD,
+	};
+
+	private HexLocation[] landLocations = {
+		new HexLocation(-2,  0),
+		new HexLocation(-2,  1),
+		new HexLocation(-2,  2),
+		new HexLocation(-1, -1),
+		new HexLocation(-1,  0),
+		new HexLocation(-1,  1),
+		new HexLocation(-1,  2),
+		new HexLocation( 0, -2),
+		new HexLocation( 0, -1),
+		new HexLocation( 0,  0),
+		new HexLocation( 0,  1),
+		new HexLocation( 0,  2),
+		new HexLocation( 1, -2),
+		new HexLocation( 1, -1),
+		new HexLocation( 1,  0),
+		new HexLocation( 1,  1),
+		new HexLocation( 2, -2),
+		new HexLocation( 2, -1),
+		new HexLocation( 2,  0),
+	};
+	
+	private transient final ResourceType[] hexTypes = {
+		ResourceType.ORE,
+		ResourceType.WHEAT,
+		ResourceType.WOOD,
+
+		ResourceType.BRICK,
+		ResourceType.SHEEP,
+		ResourceType.SHEEP,
+		ResourceType.ORE,
+
+		null,
+		ResourceType.WOOD,
+		ResourceType.WHEAT,
+		ResourceType.WOOD,
+		ResourceType.WHEAT,
+
+		ResourceType.BRICK,
+		ResourceType.ORE,
+		ResourceType.BRICK,
+		ResourceType.SHEEP,
+
+		ResourceType.WOOD,
+		ResourceType.SHEEP,
+		ResourceType.WHEAT,
+	};
+	
+	private transient final int[] hexNumbers = {
+		5, 2, 6,
+		8, 10, 9, 3,
+		/*0,*/ 3, 11, 4, 8,
+		4, 9, 5, 10,
+		11, 12, 6
+	};
+	
 	public Map(boolean randomTiles, boolean randomPorts, boolean randomNumbers){
 		//TODO: Create a map with these attributes.
+		super();
+		this.hexes = new ArrayList<Hex>();
+		this.ports = new ArrayList<Port>();
+		this.settlements = new ArrayList<Settlement>();
+		this.cities = new ArrayList<City>();
+		this.radius = 3;
+		this.robber = new Robber(3, 3);
+        this.roads = new ArrayList<Road>();
+		
+		List<Integer> numbers = new ArrayList<>(this.hexNumbers.length);
+		for (int i : this.hexNumbers) {
+			numbers.add(i);
+		}
+		List<ResourceType> hexTypes = Arrays.asList(this.hexTypes);
+		List<ResourceType> portTypes = Arrays.asList(this.portTypes);
+
+		if (randomNumbers) {
+			Collections.shuffle(numbers);
+		}
+
+		if (randomPorts) {
+			Collections.shuffle(portTypes);
+		}
+
+		if (randomTiles) {
+			Collections.shuffle(hexTypes);
+		}
+		
+		//land tiles
+		int desert_offset = 0;
+		for (int i = 0; i < landLocations.length; i++) {
+			Hex tempHex = new Hex();
+            tempHex.setLocation(landLocations[i]);
+            tempHex.setResource(hexTypes.get(i));
+			tempHex.setHasRobber(false);
+            if (hexTypes.get(i) == null) {
+				tempHex.setHasRobber(true);
+                this.robber = new Robber(landLocations[i].getX(), landLocations[i].getY());
+                desert_offset = -1;
+            } else {
+                tempHex.setNumber(numbers.get(i + desert_offset));
+            }
+            this.hexes.add(tempHex);
+        }
+		
+		//ports
+		for (int i = 0; i < portLocations.length; i++) {
+			Port tempPort = new Port();
+			tempPort.setResource(portTypes.get(i));
+			tempPort.setDirection(portDirections[i]);
+			tempPort.setLocation(portLocations[i]);
+			int ratio = 4;
+			if (portTypes.get(i) == null){
+				ratio = 3;
+			} else {
+				ratio = 2;
+			}
+			tempPort.setRatio(ratio);
+			this.ports.add(tempPort);
+		}
+		
 	}
 	
     public Map() {
